3a86d68deb36875dce22ef8f0fbae8b715cb6d11
Elias Wennerlund <elias06wennerlund@gmail.com>
2023-04-02 10:52:05 +0200

    Fixing mistake

--- content/src/functions/foodMenu.js
+++ content/src/functions/foodMenu.js
@@ -13,8 +13,7 @@ export async function toggleFood() {
 
 export async function getMenu() {
 	const data = await $.get('https://skolmaten.se/api/3/menu/?school=4806606910914560&limit=2&offset=0&client=j44i0zuqo8izmlwg5blh');
-	// const todayIndex = new Date().getDay() - 1;
-	const todayIndex = 0;
+	const todayIndex = new Date().getDay() - 1;
 	const meals = data.weeks[0].days[todayIndex].meals.map(meal => meal.value);
 	const imgURL = data.school.imageURL;
 	const schoolName = data.school.name;
